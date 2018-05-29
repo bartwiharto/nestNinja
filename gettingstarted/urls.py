91c45cca50046f0957302c786f3c566a44fd7590
bartwiharto <bartwiharto@gmail.com>
2018-05-29 16:23:24 -0600

    first commit

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -11,7 +11,7 @@ import hello.views
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.hompage, name='homepage'),
+    url(r'^$', hello.views.homepage, name='homepage'),
     url(r'^db', hello.views.db, name='db'),
     path('admin/', admin.site.urls),
 ]
