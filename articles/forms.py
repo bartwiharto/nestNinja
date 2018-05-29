110e43a0d083aab3c1c919f7113575063389ad07
bartwiharto <bartwiharto@gmail.com>
2018-05-29 14:53:45 -0600

    third commit

--- articles/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from. import models
-
-
-class CreateArticles(forms.ModelForm):
-    class Meta:
-        model = models.Articles
-        fields = ['title', 'body', 'slug']
